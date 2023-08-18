5ecc82358c6d9917a39087984a8b229f8444dd96
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-08-18 16:56:53 +0530

    Initial Commit

---
+++ ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Shivansh Gupta on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
